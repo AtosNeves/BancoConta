c0e4d0021ec4ee3db09b246d771db7d400021323
Atos Neves <72328261+AtosNeves@users.noreply.github.com>
2023-08-12 21:45:44 -0300

    Inicio

---
+++ src/Banco.java
@@ -0,0 +1,59 @@
+public class Banco {
+
+    private int numero;
+    private  String agencia;
+    private  String nomeCliente;
+
+    private Float saldo;
+
+
+    public Banco(int numero,String agencia, String nomeCliente, Float saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+
+
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public Float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Float saldo) {
+        this.saldo = saldo;
+    }
+
+
+
+
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int cod;
+        String primeiroNome;
+        String ultimoNome;
+
+        List<Banco> contas = new ArrayList<>();
+
+
+        for (int i = 0; i < 2; i++) {
+            cod = input.nextInt();
+            primeiroNome = input.next();
+            ultimoNome = input.next();
+            //Banco conta = new Banco();
+            //contas.add(conta);
+        }
+
+
+
+
+
+    }
+}
